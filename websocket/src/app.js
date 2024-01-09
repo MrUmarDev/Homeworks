fdbc17c88baebd36eb77d0763acb9d43a562f878
MrUmarDev <mrumardev@gmail.com>
2024-01-09 11:29:25 +0500

    Websocket2

--- websocket/src/app.js
+++ websocket/src/app.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const http = require('http');
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 
 // LOCAL MODULES
 const IO = require('./utils/io');
-const User = require('./models/User')
-const Group = require('./models/Group')
+const User = require('./models/User');
+const Group = require('./models/Group');
 
-const Users = new IO(process.cwd() + '/database/users.json')
-const Groups = new IO(process.cwd() + '/database/groups.json')
+const Users = new IO(process.cwd() + '/database/users.json');
+const Groups = new IO(process.cwd() + '/database/groups.json');
 
 const app = express();
 
-
 const createUser = async (username, groupname) => {
-    const users = await Users.read()
-    const id = (users[users.length - 1]?.id || 0) + 1
-    const newUser = new User(id, username)
-    newUser.userGroups.push(groupname)
-    const result = users.length ? [...users, newUser] : [newUser]
-    await Users.write(result)
-}
+    const users = await Users.read();
+    const id = (users[users.length - 1]?.id || 0) + 1;
+    const newUser = new User(id, username);
+    newUser.userGroups.push(groupname);
+    const result = users.length ? [...users, newUser] : [newUser];
+    await Users.write(result);
+};
 
 const createGroup = async (name, author) => {
     const groups = await Groups.read();
@@ -29,60 +28,63 @@ const createGroup = async (name, author) => {
     newGroup.groupUsers.push(author);
     const result = groups.length ? [...groups, newGroup] : [newGroup];
     await Groups.write(result);
-    return newGroup; // Return the new group
+    return newGroup;
 };
-
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index')
-})
+    res.render('index');
+});
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*'
-    }
+        origin: '*',
+    },
 });
 
 io.on('connection', async (socket) => {
-    let users = await Users.read()
-    let groups = await Groups.read()
-    let info = await Groups.read()
-    io.on('disconnect', () => {})
-
-    // CREATE SECTION
+    let users = await Users.read();
+    let groups = await Groups.read();
 
     socket.on('createGroup', async (data) => {
-        const findAuthor = users.find(user => user.username === data.authorName);
+        const findAuthor = users.find((user) => user.username === data.authorName);
         if (!findAuthor) {
-            createUser(data.authorName, data.groupName)
-            const findGroup = groups.find(group => group.name === data.groupName);
-            if (!findGroup) {
-                await createGroup(data.groupName, data.authorName)
-                socket.join(data.groupName)
-                socket.to('Chat1').emit('new-message', {message: `${data.authorName} joined to the group ${data.groupName}`})
-            }
-            info = await Groups.read()
-            io.emit('groups', {info})
-        }
-        else {
-            const findGroup = groups.find(group => group.name === data.groupName);
-            if (!findGroup) {
-                await createGroup(data.groupName, data.authorName)
-                socket.join(data.groupName)
-                socket.to('Chat2').emit('new-message', {message: `${data.authorName} joined to the group ${data.groupName}`})
-            }
-            info = await Groups.read()
-            io.emit('groups', {info})
-        }
-    })
+            createUser(data.authorName, data.groupName);
+            const newGroup = await createGroup(data.groupName, data.authorName);
+            socket.join(data.groupName);
 
+            socket.emit('existing-messages', { groupName: data.groupName, messages: newGroup.messages });
 
-    io.emit('groups', {info})
-} )
+            io.to(data.groupName).emit('new-message', { message: `${data.authorName} joined to the group ${data.groupName}` });
+        } else {
+            const findGroup = groups.find((group) => group.name === data.groupName);
+            if (!findGroup) {
+                const newGroup = await createGroup(data.groupName, data.authorName);
+                socket.join(data.groupName);
+
+                socket.emit('existing-messages', { groupName: data.groupName, messages: newGroup.messages });
+
+                io.to(data.groupName).emit('new-message', { message: `${data.authorName} joined to the group ${data.groupName}` });
+            }
+        }
+    });
+
+    socket.on('send-message', (data) => {
+        const { groupName, message, author } = data;
+
+        const group = groups.find((group) => group.name === groupName);
+        if (group) {
+            group.messages.push({ author, message });
+
+            io.to(groupName).emit('new-message', { message, author });
+        }
+    });
+
+    io.emit('groups', { info: groups });
+});
 
 server.listen(5555, () => {
     console.log(5555);
